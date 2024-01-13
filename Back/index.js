38678d8b9dd6b3b5ae2dd67e763eaf0af7cd309b
paulov-dev <paulovmmf@gmail.com>
2024-01-12 23:26:37 -0300

    DB

--- Back/index.js
+++ Back/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 
-//ROTAS
+// TESTAR CONEXÃO COM DB
+const db = require('./db/models')
 
+// CONTROLLERS
+const users = require('./controllers/users');
 
+// ROTAS
+app.use('/', users);
 
+// START
 app.listen(3000, () => {
     console.log('Servidor online na porta 3000 http://localhost:3000')
 })
